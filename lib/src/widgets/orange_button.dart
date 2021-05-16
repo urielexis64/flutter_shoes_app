b012a7c64bbc837fa875ccbe92041c7fa8ede014
Uriel Aispuro <urielalexis64@gmail.com>
2021-05-16 15:21:20 -0600

    width, height and color parameters

--- lib/src/widgets/orange_button.dart
+++ lib/src/widgets/orange_button.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 
 class OrangeButton extends StatelessWidget {
   final String text;
+  final double width;
+  final double height;
+  final double opacity;
 
-  OrangeButton({@required this.text});
+  OrangeButton(
+      {@required this.text,
+      this.width = 100,
+      this.height = 40,
+      this.opacity = 1});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
-      height: 40,
+      width: this.width,
+      height: this.height,
       alignment: Alignment.center,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100), color: Colors.orangeAccent),
+          borderRadius: BorderRadius.circular(100),
+          color: Colors.orangeAccent.withOpacity(opacity)),
       child: Text(
         text,
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
